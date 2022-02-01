f12915278a61f2167c69ace2705b43459b85669c
Ruslan Panenko <panenko.ruslan1@gmail.com>
2022-02-01 17:59:37 +0200

    01 february1

--- src/myshop/myshop/settings/_base.py
+++ src/myshop/myshop/settings/_base.py
@@ -133,3 +133,18 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CART_SESSION_ID = 'cart'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+BRAINTREE_MERCHANT_ID = 'XXX'
+BRAINTREE_PUBLIC_KEY = 'XXX'
+BRAINTREE_PRIVATE_KEY = 'XXX'
+
+
+import braintree
+
+BRAINTREE_CONF = braintree.Configuration(
+	braintree.Environment.Sandbox,
+	BRAINTREE_MERCHANT_ID,
+	BRAINTREE_PUBLIC_KEY,
+	BRAINTREE_PRIVATE_KEY
+)
